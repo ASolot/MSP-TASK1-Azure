c2a2739fb4151a94670fe1bec224c6604ccd9da8
Alexandru Solot <solot.alexandru@yahoo.co.uk>
2016-03-31 19:17:57 +0300

    Created EmployeeContext.cs

---
+++ MSP-TASK1-Azure/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSP_TASK1_Azure
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext() : base("name = EmployeeDbConnectionString")
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
