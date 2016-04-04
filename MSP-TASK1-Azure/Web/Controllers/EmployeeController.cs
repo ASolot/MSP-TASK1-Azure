caa8d4640a405e3f051cee02973acddf26d9e8a7
Alexandru Solot <solot.alexandru@yahoo.co.uk>
2016-04-04 22:21:08 +0300

    Created WebApp (Model, Controller, Hub, index.html)

---
+++ MSP-TASK1-Azure/Web/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web.Hubs;
+using WebApp.Models;
+
+namespace Web.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+
+        public static List<Employee> Employees = new List<Employee>()
+        {
+            new Employee(5, "Gheorghe", "Praschiv", "BR5", 25, 123)
+        };
+
+        [HttpGet]
+        public List<Employee> GetEmployees()
+        {
+            return Employees;
+        }
+
+        [HttpPost]
+        public void AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+            GlobalHost.ConnectionManager.
+                GetHubContext<EmployeeHub>().Clients.All.publishEmployee(employee);
+        }
+    }
+}
---
+++ MSP-TASK1-Azure/Web/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int employeeID, string firstName, string lastName,
+                            string address, int age, double salary)
+        {
+            EmployeeID = employeeID;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            Age = age;
+            Salary = salary;
+        }
+
+        public Employee()
+        {
+
+        }
+    }
+}
+
